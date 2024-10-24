e7d27d9c0011a11fa407509a8c44b5965646f5e5
Brenda Sayonara <contatobrendasayonara@gmail.com>
2024-10-23 22:48:32 -0400

    Create page-components.js

---
+++ scripts/page-components.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+    // Head
+    $.get('components/head.html', function(data) {
+        $('head').append(data);
+
+        // Title
+        var pageKey = $('body').data('page-key');
+        var pageTitle = pageConfig.pageTitles[pageKey]
+        document.title + pageConfig.titelPrefix + pageTitle;
+    });
+
+    // Header
+    $('body').prepend($('<header>').load('components/header.html'));
+
+    // Footer
+    $('body').append($('<footer>').load('components/footer.html'));
+});
