31873829cda1c4f402adf930dea83f192cd1ebde
Brenda Sayonara <contatobrendasayonara@gmail.com>
2024-10-23 22:48:31 -0400

    Delete header-footer.js

--- scripts/header-footer.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    // Header
-    $('body').prepend($('<header>').load('components/header.html'));
-
-    // Footer
-    $('body').append($('<footer>').load('components/footer.html'));
-});
