b5d0e3d0486c7ac0407d9c94e1f7879d4e9a9dae
Brenda Sayonara <contatobrendasayonara@gmail.com>
2024-10-23 22:48:34 -0400

    Create page-titles.js

---
+++ scripts/page-titles.js
@@ -0,0 +1,9 @@
+var pageConfig = {
+    titlePrefix: "Brenda Oliveira's Brave Owl | ",
+    pageTitles: {
+        home: "Home",
+        about: "Introduction",
+        contract: "Contract",
+        brand: "Brand Guide"
+    }
+}
