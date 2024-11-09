d3517b20b86f1cffc8daf673bc4650ab5f78b249
Brenda Sayonara <contatobrendasayonara@gmail.com>
2024-11-09 03:02:50 -0500

    Update fizzbuzz.js

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -25,5 +25,5 @@ document.getElementById('fizzbuzz').addEventListener('submit', function(event) {
             output += i + '. Brave Owl - The number is odd <br>';
         }
     }
-    document.getElementById('fizzBuzzOutput').innerHTML = output;
+    document.getElementById('fizzOutput').innerHTML = output;
 });
