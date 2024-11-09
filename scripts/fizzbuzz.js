0c4602cf7ee92490baef7fb51eda883c06304578
Brenda Sayonara <contatobrendasayonara@gmail.com>
2024-11-09 03:00:41 -0500

    Create fizzbuzz.js

---
+++ scripts/fizzbuzz.js
@@ -0,0 +1,29 @@
+document.getElementById('fizzbuzz').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Inputs
+    var firstName = document.getElementById('first-name').value;
+    var middleInitial = document.getElementById('middle-initial').value;
+    var lastName = document.getElementById('last-name').value;
+
+    // Greeting message
+    var fullName = firstName + " " + middleInitial + ". " + lastName;
+    var welcome = "Welcome to Brave Owl's FizzBuzz, " + fullName + "!";
+
+    // Greeting Updated
+    document,getElementById('greeting').textContent = welcome;
+
+    // Prompt for a number
+    var countLimit = prompt("How hight do you want to count, " + firstName + "?");
+
+    // FizzBuzz
+    var output = '';
+    for (let i = 1; i<= countLimit; i++) {
+        if (i % 2 === 0) {
+            output += i + '. Brave Owl - The number is ever <br>';
+        } else {
+            output += i + '. Brave Owl - The number is odd <br>';
+        }
+    }
+    document.getElementById('fizzBuzzOutput').innerHTML = output;
+});
