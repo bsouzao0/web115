ec8fd1e5324fe3609934304dcbbca6fe228d1d5f
Brenda Sayonara <contatobrendasayonara@gmail.com>
2024-11-20 17:32:51 -0500

    Update fizzbuzz1.js

--- scripts/fizzbuzz1.js
+++ scripts/fizzbuzz1.js
@@ -13,17 +13,9 @@ document.getElementById('fizzbuzz1').addEventListener('submit', function(event) 
     // Greeting Updated
     document.getElementById('greeting').textContent = welcome;
 
-    // Prompt for a number
-    var countLimit = prompt("How high do you want to count, " + firstName + "?");
-
     // FizzBuzz 1
-    var output = '';
-    for (let i = 1; i<= countLimit; i++) {
-        if (i % 2 === 0) {
-            output += i + '. Brave Owl - The number is even <br>';
-        } else {
-            output += i + '. Brave Owl - The number is odd <br>';
-        }
+    function createList() {
+        const word = document.get
     }
     document.getElementById('fizz-output').innerHTML = output;
 });
